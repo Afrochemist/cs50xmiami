52b9f5d44c1af8803a02968f4963a852d0c4f705
Allan Brooks <Allan Brooks>
2016-05-17 20:26:49 -0400

    just started pset 3

--- psets/3.js
+++ psets/3.js
@@ -9,4 +9,15 @@ http://math2.org/math/geometry/circles.htm
 - Calculate the area based on the radius, and output "The area is NN".
 */
 
-// write your solution here...
+// write your solution help
+ 
+ var radius = 4;
+
+ var circumference = 2 * 3.14 * radius;
+
+ console.log(circumference);
+
+ var distance = 2;
+ var area = 3.14 * distance;
+
+ console.log(area);
